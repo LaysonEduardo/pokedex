bfeae78fb35c5db6f7bac5fe1ccb99d3538107a0
LaysonEduardo <layson.contato@gmail.com>
2023-07-08 13:44:31 -0300

    fix: fixed pokemon color management

--- lib/src/models/pokemon/pokemon_model.dart
+++ lib/src/models/pokemon/pokemon_model.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/widgets.dart';
-import 'package:json_annotation/json_annotation.dart';
-import 'package:palette_generator/palette_generator.dart';
 // import 'pokemon_form_model.dart';
 
-@JsonSerializable()
+import 'package:flutter/material.dart';
+
 class Pokemon {
   int id;
   String name;
@@ -12,8 +10,7 @@ class Pokemon {
   // List<String> types;
   int height;
   int weight;
-  PaletteColor? dominant_color;
-  Image? image_provider;
+  late Color mainColor;
   Pokemon({
     required this.id,
     required this.name,
@@ -31,7 +28,6 @@ class Pokemon {
       sprite_url: json['sprites']['front_default'],
       artwork_url: json['sprites']['other']['official-artwork']
           ['front_default'],
-      // types: List<String>.from(json['types']),
       height: json['height'],
       weight: json['weight'],
     );
--- lib/src/utils/app_colors.dart
+++ lib/src/utils/app_colors.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:palette_generator/palette_generator.dart';
 
 class AppColors {
-  Future<PaletteColor> pokemonColor(ImageProvider image) async {
+  AppColors._();
+  static Future<Color> pokemonColor(ImageProvider image) async {
     final palette = await PaletteGenerator.fromImageProvider(image);
-    final color = palette.dominantColor;
-    if (color != null) {
-      return color;
+    final paletteColor = palette.dominantColor;
+    if (paletteColor != null) {
+      return paletteColor.color;
     }
     throw Exception();
   }
